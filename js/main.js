761086af7f520b60bb48a5229e4c7c00b4ff9f08
menardj <JesseMenard@Jesses-MacBook.local>
2015-03-16 19:42:39 -0400

    JS and CSS
    Worked on linking js and css pages.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $btnCollapseExpand01 = $('.btn-collapse-expand01');
+var $panel01 = $('.panel01');
+
+$btnCollapseExpand01.on('click', function (){
+	$panel01.toggleClass ('js-collapse-expand01');
+})
+
+var $btnCollapseExpand02 = $('.btn-collapse-expand02');
+var $panel02 = $('.panel02');
+
+$btnCollapseExpand02.on('click', function (){
+	$panel02.toggleClass ('js-collapse-expand02');
+})
+
+var $btnCollapseExpand03 = $('.btn-collapse-expand03');
+var $panel03 = $('.panel03');
+
+$btnCollapseExpand03.on('click', function (){
+	$panel03.toggleClass ('js-collapse-expand03');
+})
